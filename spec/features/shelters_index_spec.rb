f60db832ebbb0dcd0dfc3c0a8bde910797ce0b04
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-07-04 00:03:31 -0600

    edit the test description.

--- spec/features/shelters_index_spec.rb
+++ spec/features/shelters_index_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "shelters/index.html.erb", type: :view do
   describe 'user visits index page' do 
     context 'happy path' do
-      it "lets the user see a list of all shelters" do
+      it "lets the user see a list of all shelter's names" do
         @shelter1 = Shelter.create!(name: "Rex's Kennel", non_profit: true, rank: 15)
         @shelter2 = Shelter.create!(name: "VCA", non_profit: false, rank: 29)
         
