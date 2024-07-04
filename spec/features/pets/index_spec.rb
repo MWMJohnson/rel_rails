b6cf1f62b3e4001dcb57cb81595cdbc506df33f8
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-07-04 01:19:19 -0600

    edit description labebling.

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "pets/index.html.erb", type: :view do
-  describe 'user visits index page' do 
+  describe 'user visits pet index page' do 
     context 'happy path' do
-      it "lets the user see a list of all shelter's names" do
+      it "lets the user see a list of all pets with their attributes" do
         @shelter1 = Shelter.create!(name: "Rex's Kennel", non_profit: true, rank: 15)
         @shelter2 = Shelter.create!(name: "VCA", non_profit: false, rank: 29)
 
@@ -11,7 +11,7 @@ RSpec.describe "pets/index.html.erb", type: :view do
         @pet2 = @shelter2.pets.create!(name:"Sam", special_needs: true, age: 12)
 
         visit pets_path
-        
+
         expect(page).to have_content("Pets")
         
         expect(page).to have_content(@pet1.name)
