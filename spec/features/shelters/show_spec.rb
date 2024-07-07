98eae53b537f85eda906e433cc89471f79b56fd2
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-07-07 17:29:18 -0600

    test: working link to shelter's pets index.

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -39,6 +39,47 @@ RSpec.describe "shelters/show.html.erb", type: :view do
         visit "/shelters/#{@shelter3.id}"
         expect(page).to have_content("Number of pets available: 0")
       end
+
+      it "has a link to a list of that shelter's pets" do
+        @shelter1 = Shelter.create!(name: "Rex's Kennel", non_profit: true, rank: 15)
+        @pet1 = @shelter1.pets.create!(name:"Sam", special_needs: false, age: 12)
+
+        @shelter2 = Shelter.create!(name: "VCA", non_profit: false, rank: 29)
+        @pet2 = @shelter2.pets.create!(name:"Max", special_needs: true, age: 3)
+        @pet3 = @shelter2.pets.create!(name:"Freddy", special_needs: true, age: 15)
+
+        visit "/shelters/#{@shelter2.id}"
+
+        click_on 'Pets available at this shelter'
+        
+        expect(page).to have_current_path "/shelters/#{@shelter2.id}/pets"
+
+        expect(page).to have_content(@pet2.name)
+        expect(page).to have_content("Max")
+
+        expect(page).to have_content(@pet2.id)
+
+        expect(page).to have_content(@pet2.special_needs)
+        expect(page).to have_content(true)
+
+        expect(page).to have_content(@pet2.age)
+        expect(page).to have_content(3)
+
+        expect(page).to have_content(@pet2.shelter.name)
+        expect(page).to have_content("VCA")
+
+        expect(page).to have_content(@pet3.name)
+        expect(page).to have_content(@pet3.id)
+        expect(page).to have_content(@pet3.special_needs)
+        expect(page).to have_content(@pet3.age)
+        expect(page).to have_content(@pet3.shelter.name)
+
+        expect(page).to_not have_content(@pet1.name)
+        expect(page).to_not have_content(@pet1.id)
+        expect(page).to_not have_content(@pet1.special_needs)
+        expect(page).to_not have_content(@pet1.age)
+        expect(page).to_not have_content(@pet1.shelter.name)
+      end
     end
   end
   
