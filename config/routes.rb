3b76bdf921e4cf72525ab0ce6a9ac04b747ab7d4
Matthew Johnson <matthewwmjohnson@gmail.com>
2024-07-30 15:43:22 -0600

    add a route for destroying a shelter.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/shelters/:id", to: "shelters#show"
   get "/shelters/:id/edit", to: "shelters#edit"
   patch "/shelters/:id", to: "shelters#update"
+  delete "/shelters/:id", to: "shelters#destroy"
   
   get "/pets", to: "pets#index", as: "pets"
   get "/pets/:id", to: "pets#show"
